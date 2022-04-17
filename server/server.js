9da19496976868339adebf41ee6d7ad9b5e6214a
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-16 21:09:21 -0500

    server1

---
+++ server/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const db = require('./db')
+
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//Get all restaurants
+
+
+
+//Get a restaurant
+
+
+
+//Create a restaurant
+
+
+
+//Update restaurants
+
+
+
+//Delete restaurant
