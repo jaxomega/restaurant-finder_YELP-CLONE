3bae2713e87c5f1fe43133e1cff537999c27fde7
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-18 12:55:50 -0500

    getallvariable

--- server/server.js
+++ server/server.js
@@ -11,7 +11,22 @@ app.use(cors())
 app.use(express.json())
 
 //Get all restaurants
-
+app.get('/api/v1/restaurants', async (req, res) => {
+    try {
+        const restaurantRatingsData = await db.query(
+            "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id"
+        )
+        res.status(200).json({
+            status: 'success',
+            results: restaurantRatingsData.rows.length,
+            data: {
+                restaurants: restaurantRatingsData.rows,
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+});
 
 
 //Get a restaurant
