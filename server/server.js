25958a6c29c1eeb807e5a1a266bb0f71efedb738
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-18 13:08:26 -0500

    getavariable

--- server/server.js
+++ server/server.js
@@ -28,9 +28,32 @@ app.get('/api/v1/restaurants', async (req, res) => {
     }
 });
 
-
 //Get a restaurant
+app.get('/api/v1/restaurants/:id', async (req, res) => {
+    console.log(req.params.id)
 
+    try {
+        const restaurant = await db.query(
+            "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1"
+            [req.params.id]
+        )
+        const reviews = await db.query(
+            "select * from reviews where restaurant_id = $1"
+            [req.params.id]
+    )
+    console.log(reviews)
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            restaurant: restaurant.row[0],
+            reviews: reviews.rows,
+        }
+    })
+} catch (err)  {
+    console.log(err)
+}
+})
 
 
 //Create a restaurant
