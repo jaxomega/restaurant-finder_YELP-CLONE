2bbba65030e8f90782950b3586fbe5ba48d2d777
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-18 14:15:41 -0500

    restaurantcontext

---
+++ client/src/context/RestaurantContext.js
@@ -0,0 +1,24 @@
+import React, { useState, createContext } from 'react'
+
+export const RestaurantsContext = createContext();
+
+export const RestaurantsContextProvider = (props) => {
+    const [restaurants, setRestaurants] = useState([])
+    const [selectedRestaurants, setSelectedRestaurant] = useState(null)
+    const addRestaurants = (restaurants) => {
+        setRestaurants([...restaurants, restaurant])
+    };
+    return (
+        <RestaurantsContext.Provider
+            value={{
+                restaurants,
+                setRestaurants,
+                addRestaurants,
+                selectedRestaurants,
+                setSelectedRestaurant
+            }}
+        >
+            {props.children}
+        </RestaurantsContext.Provider>
+    )
+};
