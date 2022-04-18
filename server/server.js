67dc22e6769281e276b6afa571b545c06a1af5f2
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-18 13:37:52 -0500

    deletevariable

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ app.use(express.json())
 
 //Get all restaurants
 app.get('/api/v1/restaurants', async (req, res) => {
+    
     try {
         const restaurantRatingsData = await db.query(
             "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id"
@@ -34,11 +35,11 @@ app.get('/api/v1/restaurants/:id', async (req, res) => {
 
     try {
         const restaurant = await db.query(
-            "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1"
+            "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1",
             [req.params.id]
         )
         const reviews = await db.query(
-            "select * from reviews where restaurant_id = $1"
+            "select * from reviews where restaurant_id = $1",
             [req.params.id]
     )
     console.log(reviews)
@@ -46,14 +47,14 @@ app.get('/api/v1/restaurants/:id', async (req, res) => {
     res.status(200).json({
         status: 'success',
         data: {
-            restaurant: restaurant.row[0],
-            reviews: reviews.rows,
+            restaurant: restaurant.rows[0],
+            reviews: reviews.rows
         }
     })
 } catch (err)  {
     console.log(err)
 }
-})
+});
 
 //Create a restaurant
 app.post('/api/v1/restaurants', async (req, res) => {
@@ -61,23 +62,76 @@ app.post('/api/v1/restaurants', async (req, res) => {
 
     try {
         const results = await ddb.query(
-            "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *"
+            "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
             [req.body.name, req.body.location, req.body.price_range]
         )
         console.log(results)
         res.status(201).json({
             status: 'success',
             data: {
-                restaurant: results.row[0]
+                restaurant: results.rows[0]
             }
         })
     } catch (err) {
         console.log(err)
     }
-})
+});
 
 //Update restaurants
-
-
+app.put('/api/v1/restaurants/:id', async (req, res) => {
+    
+    try {
+      const results = await db.query(
+        "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+        [req.body.name, req.body.location, req.body.price_range, req.params.id]
+      )
+        res.status(200).json({
+        status: 'succes',
+        data: {
+          retaurant: results.rows[0]
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
+    console.log(req.params.id)
+    console.log(req.body)
+  });
 
 //Delete restaurant
+app.delete('/api/v1/restaurants/id:', async (req, res) => {
+    try {
+     const results = db.query(
+        "DELETE FROM restaurants where id = $1",
+        [req.params.id]
+     )
+        res.status(204).json({
+            status: 'success'
+        })
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+app.post('/api/v1/restaurants/:id/addReview', async (req, res) => {
+    try {
+        const newReview = await db.query(
+            "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *",
+            [req.params.id, req.body.name, req.body.review, req.body.rating]
+        )
+        console.log(newReview)
+        res.status(201).json({
+            status: 'success',
+            data: {
+                review: newReview.rows[0]
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    console.log(`server is up and listening on port ${port}`)
+});
