ae2002485d51b5d68680eebf076a2669cd7de363
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-19 12:55:21 -0500

    header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="font-light">
+                Restaurant Finder
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
