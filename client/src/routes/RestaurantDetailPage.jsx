125b613079a6639ea0dc4473729405a87e3d7d22
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-19 14:05:35 -0500

    routedpmore

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -6,3 +6,48 @@ import StarRating from '../components/StarRating'
 import Reviews from '../components/Reviews'
 import AddReview from '../components/AddReview'
 
+const RestaurantDetailPage = () => {
+    const { id } = useParams()
+    const{ slectedRestaurant, setSelectedRestaurant } = useContext(
+        RestaurantContext
+    );
+    useEffect(() => {
+       const fetchData = async () => {
+           try {
+           const response = await RestaurantFinder.get(`/${id}`)
+           console.log(response);
+           
+           setSelectedRestaurant(response.data.data)
+       } catch (err) {
+           console.log(err);
+       }
+    };
+
+        fetchData()
+    }, []);
+    return (
+        <div>
+            {selectedRestaurant && (
+                <>
+                <h1 className="text-center display">
+                    {selectedRestaurant.restaurant.name}
+                </h1>
+                <div className="text-center">
+                    <StarRating rating={selectedRestaurant.restaurant.average_rating} />
+                    <span className="text-warning">
+                        {selectedRestaurant.restaurant.count
+                        ? `(${selectedRestaurant.restaurant.count})`
+                        : "(0)"}
+                    </span>
+                </div>
+                <div className="mt-3">
+                    <Reviews reviews={selectedRestaurant.reviews} />
+                </div>
+                <AddReview />
+                </>
+            )}
+        </div>
+    );
+};
+
+export default RestaurantDetailPage;
