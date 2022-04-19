2e9fa6d7f188585bbde79e5073af375631a31506
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-19 10:47:19 -0500

    addrevcont

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -40,9 +40,40 @@ const AddReview = () => {
                             className="form-control"
                             />
                     </div>
-                    
+                    <div className="form-group col-4">
+                        <label htmlFor="rating">Rating</label>
+                        <select
+                            value={rating}
+                            onChnage={(e) => setRating(e.target.value)}
+                            id="rating"
+                            className="custom-select"
+                            >
+                                <option disabled>Rating</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                    </div>
                 </div>
+                <div className="form-group">
+                    <label htmlFor="Review">Review</label>
+                    <textarea
+                        value={reviewText}
+                        onChange={(e) => setReviewText(e.target.value)}
+                        id="Review"
+                        className="form-control"
+                        ></textarea>
+                </div>
+                <button
+                    type="submit"
+                    onClick={handleSubmitReview}
+                    className="btn-primary"
+                    >Submit</button>
             </form>
         </div>
-    )
-}
+    );
+};
+
+export default AddReview;
