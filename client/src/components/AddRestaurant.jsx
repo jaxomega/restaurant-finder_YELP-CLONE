130e93df60a9c80e1fd131070b65597df2fa7175
Jackson Christopher <kitchen.king.95@gmail.com>
2022-04-19 09:55:30 -0500

    addresmore

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -22,17 +22,50 @@ const AddRestaurant = () => {
         }
     };
     return (
-        <div className="">
+        <div className="mb">
             <form action="">
                 <div className="form-row">
                     <div className="col">
                         <input
                             value={name}
                             onChange={(e) => setName(e.target.value)}
+                            type="text"
+                            className="form-control"
+                            placeholder="name"
                             />
                     </div>
+                    <div className="col">
+                        <input
+                            value={location}
+                            onChange={(e) => setlocation(e.target.value)}
+                            className="form-control"
+                            type="text"
+                            placeholder="location"
+                        />
+                    </div>
+                    <div className="col">
+                        <select
+                            value={priceRange}
+                            onChange={(e) => setPriceRange(e.target.value)}
+                            className="custom-select"
+                            >
+                                <option disabled>Price Range</option>
+                                <option value="1">$</option>
+                                <option value="2">$$</option>
+                                <option value="3">$$$</option>
+                                <option value="4">$$$$</option>
+                                <option value="5">$$$$$</option>
+                            </select>
+                    </div>
+                    <button
+                    onClick={handleSubmit}
+                    type="submit"
+                    className="btn-primary"
+                    >Add</button>
                 </div>
             </form>
         </div>
-    )
-}
+    );
+};
+
+export default AddRestaurant;
